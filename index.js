cfc6da8b4fa6f3f4ca61bb1e1fc120fbd84b8540
HARSH JAIN <jainharsh270@gmail.com>
2021-01-21 15:36:47 +0530

    verify and validate token

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const speakeasy = require("speakeasy");
 const uuid = require("uuid");
 const { JsonDB } = require("node-json-db");
 const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
+
 const app = express();
+
+app.use(express.json());
 
 // init db
 const db = new JsonDB(new Config("myDatabase", true, false, "/"));
@@ -20,10 +23,65 @@ app.post("/api/register", (req, res) => {
     db.push(path, { id, temp_secret });
     res.json({ id, secret: temp_secret.base32 });
   } catch (error) {
-      console.log(error)
-      res.status(500).json({message : "Error generating the secret"})
+    console.log(error);
+    res.status(500).json({ message: "Error generating the secret" });
   }
 });
+
+// Verify token and make secret permanent
+app.post("/api/verify", (req, res) => {
+  const { userId, token } = req.body;
+  try {
+    // Retrieve user from database
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+    console.log({ user });
+    const { base32: secret } = user.temp_secret;
+    const verified = speakeasy.totp.verify({
+      secret,
+      encoding: "base32",
+      token,
+    });
+    if (verified) {
+      // Update user data
+      db.push(path, { id: userId, secret: user.temp_secret });
+      res.json({ verified: true });
+    } else {
+      res.json({ verified: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving user" });
+  }
+});
+
+// validate token
+
+app.post("/api/validate", (req,res) => {
+    const { userId, token } = req.body;
+    try {
+      // Retrieve user from database
+      const path = `/user/${userId}`;
+      const user = db.getData(path);
+      console.log({ user })
+      const { base32: secret } = user.secret;
+      // Returns true if the token matches
+      const tokenValidates = speakeasy.totp.verify({
+        secret,
+        encoding: 'base32',
+        token,
+        window: 1
+      });
+      if (tokenValidates) {
+        res.json({ validated: true })
+      } else {
+        res.json({ validated: false})
+      }
+    } catch(error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error retrieving user'})
+    };
+  })
 
 const PORT = process.env.PORT || 5000;
 
