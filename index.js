c6371d4a5ebe14d1aec2a4141e0cedde37d8cf2c
HARSH JAIN <jainharsh270@gmail.com>
2021-01-21 15:19:29 +0530

    generating secret and storing in db

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ const db = new JsonDB(new Config("myDatabase", true, false, "/"));
 
 app.get("/api", (req, res) => res.json({ message: "Two Factor Auth" }));
 
+// Register user & create temp secret
+app.post("/api/register", (req, res) => {
+  const id = uuid.v4();
+
+  try {
+    const path = `/user/${id}`;
+    const temp_secret = speakeasy.generateSecret();
+    db.push(path, { id, temp_secret });
+    res.json({ id, secret: temp_secret.base32 });
+  } catch (error) {
+      console.log(error)
+      res.status(500).json({message : "Error generating the secret"})
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running on  http://localhost:${PORT}/`)
+  console.log(`Server running on http://localhost:${PORT}/api`)
 );
