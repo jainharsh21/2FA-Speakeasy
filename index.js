066df1870ac864d880a53ade6f7c192b9da743a9
HARSH JAIN <jainharsh270@gmail.com>
2021-01-21 15:11:32 +0530

    init db

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { JsonDB } = require("node-json-db");
 const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
 const app = express();
 
+// init db
+const db = new JsonDB(new Config("myDatabase", true, false, "/"));
+
 app.get("/api", (req, res) => res.json({ message: "Two Factor Auth" }));
 
 const PORT = process.env.PORT || 5000;
